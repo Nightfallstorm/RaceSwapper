2e1d5e37d67ebb1009e75e86e3d5b38631c7318b
nightfallstorm <109926124+Nightfallstorm@users.noreply.github.com>
2024-03-03 03:07:08 -0500

    Fixed body part hook

--- src/Hooks.cpp
+++ src/Hooks.cpp
@@ -56,16 +56,19 @@ struct GetBodyPartDataHook
 	{
 		// a_npc WAS the race, but we kept it as Actor for our purposes >:)
 		auto actor = reinterpret_cast<RE::Actor*>(a_actor);
-		
+		logger::debug("Getting body part data for {:x}", actor->formID);
 		auto appearance = NPCAppearance::GetNPCAppearance(actor->GetActorBase());
 		if (appearance && appearance->isNPCSwapped) {
+			logger::debug("Using new NPC body part data");
 			return appearance->alteredNPCData.bodyPartData;
 		}
 
 		if (!actor->GetActorRuntimeData().race) {
+			logger::debug("Returning no body part data for {:x}", actor->formID);
 			return nullptr;
 		}
 
+		logger::debug("Returning default body part data for {:x}", actor->formID);
 		return func(actor->GetActorRuntimeData().race);
 	}
 
@@ -75,13 +78,16 @@ struct GetBodyPartDataHook
 	static inline void Install()
 	{
 		REL::Relocation<std::uintptr_t> load3DTarget{ RELOCATION_ID(36198, 37177), REL::VariantOffset(0x5A, 0x57, 0x5A) };
-		
 		// Remove call to replace RCX (actor) with actor's race. This lets our hook have access to the actor data
 		if (REL::Module::IsAE()) {
+			byte useActorInstructions[] = { 0x48, 0x89, 0xd9 };
 			REL::safe_fill(load3DTarget.address() - 0x14, REL::NOP, 0x7);
+			REL::safe_write(load3DTarget.address() - 0x14, useActorInstructions, 0x3);
 		} else {
 			// SE/VR
+			byte useActorInstructions[] = { 0x48, 0x89, 0xF1 }; 
 			REL::safe_fill(load3DTarget.address() - 0x11, REL::NOP, 0x7);
+			REL::safe_write(load3DTarget.address() - 0x11, useActorInstructions, 0x3);
 		}
 		
 		stl::write_thunk_call<GetBodyPartDataHook>(load3DTarget.address());
