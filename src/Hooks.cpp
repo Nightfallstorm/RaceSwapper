0ef6e6b5ba57803761c72aae7bdd725763b6f383
nightfallstorm <109926124+Nightfallstorm@users.noreply.github.com>
2024-03-31 21:40:46 -0400

    Add custom height support

--- src/Hooks.cpp
+++ src/Hooks.cpp
@@ -512,6 +512,47 @@ struct IsBeastRaceHook
 	}
 };
 
+struct HeightHook
+{
+	static float thunk(RE::TESNPC* a_self)
+	{
+		auto race = a_self->race;
+		auto appearance = NPCAppearance::GetNPCAppearance(a_self);
+		if (appearance && appearance->isNPCSwapped) {
+			race = appearance->alteredNPCData.race;
+		}
+
+		return OrigRaceLogic(a_self, race);
+	}
+
+	static inline float OrigRaceLogic(RE::TESNPC* a_npc, RE::TESRace* a_appearanceRace) {
+		if (!a_appearanceRace) {
+			return a_npc->height;
+		}
+
+		const auto sex = a_npc->GetSex();
+		switch (sex) {
+		case RE::SEX::kMale :
+		case RE::SEX::kFemale :
+			return a_appearanceRace->data.height[sex] * a_npc->height;
+		default:
+			return 0.0;
+		}
+	}
+
+	// Install our hook at the specified address
+	static inline void Install()
+	{
+		REL::Relocation<std::uintptr_t> target{ REL::VariantID(24256, 24763, 0x375F90) };
+
+		SKSE::AllocTrampoline(0x14);
+		SKSE::GetTrampoline().write_branch<5>(target.address(), thunk);
+
+		logger::info("HeightHook set at address {:x}", target.address());
+		logger::info("HeightHook set at offset {:x}", target.offset());
+	}
+};
+
 struct CopyFromTemplate
 {
 	static void thunk(RE::TESActorBaseData* a_self, RE::TESActorBase* a_template)
@@ -757,6 +798,7 @@ void hook::InstallHooks()
 	LoadSkinHook::Install();
 	PopulateGraphHook::Install();
 	IsBeastRaceHook::Install();
+	HeightHook::Install();
 	SetRaceHook::Install();
 	HasOverlaysHook::Install();
 	RE::ScriptEventSourceHolder::GetSingleton()->AddEventSink(new HandleFormDelete());
